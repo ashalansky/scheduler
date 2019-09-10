2fb23aadc92d2d63b8fa2f7b7bc31c6ef43c51fa
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-09-10 09:47:15 -0600

    separated reducer function and passed last tests. Adds collectcoverageFrom into package.json

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,50 +1,10 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
+import reducer from "reducers/application";
 
 const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
-
-const reducer = function(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day }
-    case SET_APPLICATION_DATA:
-      return {  ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
-    case SET_INTERVIEW: {
-      let appointment = {}
-      if (action.interview) {
-        appointment = {...state.appointments[action.id], interview: { ...action.interview }}
-      } else {
-        appointment = {...state.appointments[action.id], interview: action.interview }
-      }
-      const appointments = {...state.appointments, [action.id]: appointment}
-   
-    const daysArray = state.days.map((day) => {
-      for (let appointment of day.appointments) {
-        if (action.id === appointment) {
-          if (action.interview && !state.appointments[action.id].interview) {
-            return { ...day, spots: day.spots - 1 };
-          } else if (
-            !action.interview && 
-            state.appointments[action.id].interview)
-          {
-            return { ...day, spots: day.spots + 1};
-          }
-        }
-      }
-      return day
-    })
-      return { ...state,  appointments, days: daysArray }
-  }
-  default:
-    throw new Error(
-      `Tried to reduce with unsupported action type: ${action.type}`
-    );
-  }
-}
-
-
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer( reducer, {
